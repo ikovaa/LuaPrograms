dca8e2fcf9c97425710ecaca92e09af55aac5499
ikovaa <afonsomfvences@gmail.com>
2022-03-27 22:51:36 +0000

    Fixed some stuff
    Fixed weird variable naming console warnings

--- programs/pvp.lua
+++ programs/pvp.lua
@@ -20,9 +20,8 @@ local player2 = {
     wins = 0,
 }
 
-function main()
+function Main()
     math.randomseed(os.time());
-    math.random();
     round = round + 1
 
     os.execute("clear") -- os.execute("cls") on Windows
@@ -33,40 +32,40 @@ function main()
         If turn equals to 0 is the character turn 
         Otherwise it's the enemy turn
     ]]
-    if dice() > 3 then turn = 1 else turn = 0 end
-    next("play")
+    if Dice() > 3 then turn = 1 else turn = 0 end
+    Next("play")
 end
 
-function play(data)
+function Play(data)
 
     --[[
         Receive the dmg value and store it in a variable
     ]]
-    local dmg = getGamage()
+    local dmg = GetGamage()
 	    if data == 0 then
         io.write("\nplayer1 Damage "..dmg .. "\n")
-        applyDamage(player2, dmg)
+        ApplyDamage(player2, dmg)
     else
         io.write("\nplayer2 Damage ".. dmg .. "\n")
-        applyDamage(player1, dmg)
+        ApplyDamage(player1, dmg)
     end
 end
 
 
 --[[ Get random damage amount ]]
-function getGamage()
+function GetGamage()
     local dmg = math.random(20,60)
     return dmg
 end
 
-function applyDamage(data, dmg)
+function ApplyDamage(data, dmg)
     local newDamage = data.armor - dmg
     data.health = data.health + newDamage
-    next("main")
+    Next("main")
 end
 
 --[[ Determine the turn randomly ]]
-function dice()
+function Dice()
     local random = math.random(1,6)
     return random
 end
@@ -75,15 +74,15 @@ end
 --[[
     If any of the players health is lower or equal to 0 continue to win(), otherwise play again
 ]]
-function next(args)
-    if (player1.health <= 0) or (player2.health <= 0) then win() end
+function Next(args)
+    if (player1.health <= 0) or (player2.health <= 0) then Win() end
         io.write("\nPress X to continue\n")
         input = io.read()
     if (input == "X") or (input == "x") then
         if args == "play" then
-            play(turn)
+            Play(turn)
         elseif args == "main" then
-            main()
+            Main()
         end
     else return end
 end
@@ -92,7 +91,7 @@ end
 --[[ 
   Checks if any of the players health is lower or equal to 0
 ]]
-function win()
+function Win()
     io.write("\n\n--------------------------\n\n")
         if (player1.health <= 0) then
         player2.wins = player2.wins + 1
@@ -103,12 +102,12 @@ function win()
         io.write("\nplayer1 Wins\n")
         io.write("player1 now has "..player1.wins.." wins")
     end
-    reset()
+    Reset()
 end
 
 
 --[[ Resets the rounds and players health ]]
-function reset()
+function Reset()
     io.write("\nThis game took "..round.." rounds\n")
     io.write("Reseting Game\n\n")
     round = 0
@@ -117,4 +116,4 @@ function reset()
 end
 
 
-main()
+Main()
